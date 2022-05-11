6ce1b2a6dd4006a89f006afc28805d937add59ff
erange_info <84660900+erange-info@users.noreply.github.com>
2022-05-11 20:39:26 +0900

    RumiMetaCognitiveCLL
    この作品はフィクションなの、登場人物は成人しているんだよ。実在の人物団体事件とは一切関係ありません。
    UTF-8は3byteでポインタは‥‥先ずはテスト用ファンタジーをSFにするところから始めないと。
    春二で名前の話題が出たときから、予兆はあったのかも知れない。
    ”A man, a plan, a cat , a canal＿＿＿Panama?”エキスパートCプログラミング＿＿＿＿知られざるCの深層（アスキー出版局）より引用

---
+++ RumiMetaCognitiveCLL/main.cpp
@@ -0,0 +1,157 @@
+﻿#include <cstdio>
+
+jmp_buf buf;
+
+class Rumi
+{
+public:
+    Rumi();
+    ~Rumi();
+
+private:
+
+};
+
+Rumi::Rumi()
+{
+    alloca()
+    stream_novel
+    
+
+
+        
+    free()
+
+    return 1;
+}
+
+Rumi::~Rumi()
+{
+
+
+
+
+}
+
+class stream_novel:Rumi
+{
+public:
+    stream_novel:Rumi();
+    ~stream_novel:Rumi();
+
+private:
+
+};
+
+stream_novel:Rumi::stream_novel:Rumi()
+{
+
+    //　実装用
+catch check_in:;
+    //    cout << "ファイルを指定してください。\n";
+    //    cin >> char fname;
+    //    stream_novel.open('fname', 1);
+    //    r << Rumi();
+    //    if( !r == 1 ){goto check_out:;}
+
+}
+
+stream_novel:Rumi::~stream_novel:Rumi()
+{
+}
+
+int main(){
+    printf("hello from RumiMetaCognitiveCLL!\n");
+
+//テスト用
+    /*るみるみに捧げたことを確認する。*/
+    stream_conf.open( 'conf.ini', 2, 3);
+    
+    if (stream_conf)  {
+
+        /*　実行可能。　*/
+
+        cout << "check...\n";
+
+        /*前回のチェックを確認*/
+
+        for (stream_conf; *stream_conf != '\0'; *stream_conf++;) {
+
+            switch (*stream_conf != '\0') {
+
+                cace strcmp(*stream_conf, 'C') == 0 {
+                    int check = 0;
+                }
+
+                cace strcmp(*stream_conf, 'h') == 0 {
+                    check++;
+                }
+                cace strcmp(*stream_conf, 'e') == 0 {
+                    check++;
+                }
+
+                cace strcmp(*stream_conf, 'c') == 0 {
+                    check++;
+                }
+                cace strcmp(*stream_conf, 'k') == 0 {
+                    check++;
+                }
+
+                cace strcmp(*stream_conf, '=') == 0 {
+                    check++;
+                }
+
+                cace strcmp(*stream_conf, '1') == 0 {
+                    check++;
+                    if (check == 7) {
+                        stream_conf.close();
+                        throw check_in;
+                    }
+                    else{
+                        /*CheckError*/
+                        goto check_out;
+                    }
+                    
+
+                }
+            default:;
+
+            }
+
+        }
+
+    }
+
+    /*　初期値とみなし動作を確認。鋳薔薇の砦の百合の塔をるみるみに捧げる。*/
+
+    stream_novel.open('..鋳薔薇の砦の百合の塔', 1);
+
+    int r << Rumi()
+
+    /*動作したことをConfに入力*/
+    stream_conf << "hello" << "from" << "RumiMetaCognitiveCLL!\n";
+    stream_conf << "Check" << "==" << r << "\n";
+    stream_conf << "(C)" << tm_year << tm_mon << tm_dey << tm_wday << tm_hour << tm_min << "\n";
+    stream_conf.close();
+        
+        }
+        
+
+    }
+    else
+    {
+    /*　実行不可能。　*/
+    check_out:;
+    stream_conf.close();
+    
+    cout << "実行に失敗しました。";
+
+    }
+
+    return 0;
+}
+
+stream_conf.close()
+{
+    return stream_conf .();
+}
